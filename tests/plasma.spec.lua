4f8848100d5fa5e645c7f69de190baadaf580b2e
Rimuy <kevinntavares@gmail.com>
2024-02-10 15:41:35 -0300

    Add unit testing

--- tests/plasma.spec.lua
+++ tests/plasma.spec.lua
@@ -5,7 +5,6 @@ return function()
 	describe("plasma", function()
 		it("should create and destroy things", function()
 			local folder = Instance.new("Folder")
-
 			local root = Plasma.new(folder)
 
 			Plasma.start(root, function()
@@ -21,7 +20,6 @@ return function()
 
 		it("should create and destroy from a single start point", function()
 			local folder = Instance.new("Folder")
-
 			local root = Plasma.new(folder)
 
 			local function start(visible)
@@ -38,6 +36,81 @@ return function()
 			start(false)
 
 			expect(folder:FindFirstChildWhichIsA("TextButton")).to.never.be.ok()
+		end)
+
+		it("should support `useRefs` hook", function()
+			local folder = Instance.new("Folder")
+			local root = Plasma.new(folder)
+
+			local refsTable
+
+			local function DescendantComponent()
+				return Plasma.create("Frame", {
+					[Plasma.useRefs()] = "baz",
+					BackgroundColor3 = Color3.fromHex("#00f"),
+				})
+			end
+
+			local function AncestorComponent()
+				local ref = Plasma.useRefs()
+				refsTable = ref
+
+				return Plasma.create("Frame", {
+					[ref] = "foo",
+					BackgroundColor3 = Color3.fromHex("#f00"),
+
+					Plasma.create("Frame", {
+						BackgroundColor3 = Color3.fromHex("#0f0"),
+
+						DescendantComponent(),
+					})
+				})
+			end
+
+			local frame = Plasma.beginFrame(root, function(tbl)
+				expect(Plasma.useInstance(AncestorComponent)).to.equal(tbl)
+			end, refsTable)
+
+			Plasma.continueFrame(frame, function(refs, red, blue)
+				expect(refs.foo).to.be.ok()
+				expect(refs.bar).to.never.be.ok()
+				expect(refs.baz).to.be.ok()
+
+				expect(refs.foo.BackgroundColor3).to.equal(red)
+				expect(refs.baz.BackgroundColor3).to.equal(blue)
+			end, refsTable, Color3.fromHex("#f00"), Color3.fromHex("#00f"))
+
+			Plasma.finishFrame(root)
+		end)
+
+		it("should disallow `useRefs` outside `useInstance`", function()
+			local folder = Instance.new("Folder")
+			local root = Plasma.new(folder)
+
+			Plasma.start(root, function()
+				Plasma.useInstance(function()
+					return Plasma.create("Folder")
+				end)
+
+				expect(Plasma.useRefs).to.throw()
+			end)
+		end)
+
+		it("should disallow mutating the returned refs table", function()
+			local folder = Instance.new("Folder")
+			local root = Plasma.new(folder)
+
+			local refs
+
+			Plasma.start(root, function()
+				refs = Plasma.useInstance(function(ref)
+					return Plasma.create("Folder", { [ref] = "test" })
+				end)
+			end)
+
+			expect(function()
+				refs.test = true
+			end).to.throw()
 		end)
 	end)
 end
